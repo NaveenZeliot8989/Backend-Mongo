daff384330efdb19ea03423ebcc07108eabce985
NaveenZeliot8989 <naveenjm908@gmail.com>
2023-09-07 14:20:13 +0530

    db connection established

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
-})
+mongoose
+  .connect("mongodb+srv://naveenDB:Naveen123@cluster0.8oflo7x.mongodb.net/first_db?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start your Express app only after successfully connecting to MongoDB
+    app.listen(6001, () => {
+      console.log("Server is running on port 6001");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
-app.listen(6001)
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
